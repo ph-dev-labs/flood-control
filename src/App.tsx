f5545340df4783343e3bf80b94a02261df93c4b6
testedtype <mac@Macs-MacBook-Air.local>
2025-03-07 16:51:44 +0100

    fixes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from 'react';
+
+import './index.css';
+import FloodPredictionApp from './components/floodPrediction';
+
+function App() {
+  return (
+    <div className="w-full">
+      <FloodPredictionApp />
+    </div>
+  );
+}
+
+export default App;
